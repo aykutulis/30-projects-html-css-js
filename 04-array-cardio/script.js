8cf8f211b2c023bc8df2120f87f6668c19f3125b
Aykut Ulış <aykutulis@hotmail.com>
2021-09-19 14:52:27 +0300

    array cardio

---
+++ 04-array-cardio/script.js
@@ -0,0 +1,116 @@
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+];
+
+const people = [
+  'Bernhard, Sandra',
+  'Bethea, Erin',
+  'Becker, Carl',
+  'Bentsen, Lloyd',
+  'Beckett, Samuel',
+  'Blake, William',
+  'Berger, Ric',
+  'Beddoes, Mick',
+  'Beethoven, Ludwig',
+  'Belloc, Hilaire',
+  'Begin, Menachem',
+  'Bellow, Saul',
+  'Benchley, Robert',
+  'Blair, Robert',
+  'Benenson, Peter',
+  'Benjamin, Walter',
+  'Berlin, Irving',
+  'Benn, Tony',
+  'Benson, Leana',
+  'Bent, Silas',
+  'Berle, Milton',
+  'Berry, Halle',
+  'Biko, Steve',
+  'Beck, Glenn',
+  'Bergman, Ingmar',
+  'Black, Elk',
+  'Berio, Luciano',
+  'Berne, Eric',
+  'Berra, Yogi',
+  'Berry, Wendell',
+  'Bevan, Aneurin',
+  'Ben-Gurion, David',
+  'Bevel, Ken',
+  'Biden, Joseph',
+  'Bennington, Chester',
+  'Bierce, Ambrose',
+  'Billings, Josh',
+  'Birrell, Augustine',
+  'Blair, Tony',
+  'Beecher, Henry',
+  'Biondo, Frank',
+];
+
+const fifteen = inventors.filter((inventor) => inventor.year >= 1500 && inventor.year < 1600);
+
+console.table(fifteen);
+
+const fullNames = inventors.map((inventor) => `${inventor.first} ${inventor.last}`);
+console.log(fullNames);
+
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(ordered);
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
+
+const oldest = inventors.sort(function (a, b) {
+  const lastInventor = a.passed - a.year;
+  const nextInventor = b.passed - b.year;
+  return lastInventor > nextInventor ? -1 : 1;
+});
+console.table(oldest);
+
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast] = lastOne.split(', ');
+  const [bLast] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpha);
+
+const data = [
+  'car',
+  'car',
+  'truck',
+  'truck',
+  'bike',
+  'walk',
+  'car',
+  'van',
+  'bike',
+  'walk',
+  'car',
+  'van',
+  'car',
+  'truck',
+  'pogostick',
+];
+
+const transportation = data.reduce(function (obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transportation);
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 | 01  | [Drum Kit](https://github.com/aykutulis/30-projects-html-css-js/tree/master/01-drum-kit)             | [Live Demo](https://aykutulis.github.io/30-projects-html-css-js/01-drum-kit)       |
 | 02  | [Analogue Clock](https://github.com/aykutulis/30-projects-html-css-js/tree/master/02-analogue-clock) | [Live Demo](https://aykutulis.github.io/30-projects-html-css-js/02-analogue-clock) |
 | 03  | [CSS Variables](https://github.com/aykutulis/30-projects-html-css-js/tree/master/03-css-variables)   | [Live Demo](https://aykutulis.github.io/30-projects-html-css-js/03-css-variables)  |
+| 04  | [Array Cardio](https://github.com/aykutulis/30-projects-html-css-js/tree/master/04-array-cardio)     | [Live Demo](https://aykutulis.github.io/30-projects-html-css-js/04-array-cardio)   |
